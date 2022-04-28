9ce853e477118b29a81276c9419b6f9eb9764096
Charles Gabriel <charlesgabriel@hotmail.com.br>
2022-04-28 14:03:51 -0300

    implementado calculo em javascript

--- script/script.js
+++ script/script.js
@@ -13,3 +13,38 @@ $('entrada-texto').addEventListener('click', function () {
         semTexto.style.display = 'none';
     }
 });
+
+function codificar(frase) {
+    const novaFrase = frase
+        .replace(/[\be\b]/g, 'enter')
+        .replace(/[\bi\b]/g, 'imes')
+        .replace(/[\ba\b]/g, 'ai')
+        .replace(/[\bo\b]/g, 'ober')
+        .replace(/[\bu\b]/g, 'ufat');
+    return novaFrase;
+}
+function decodificar(frase) {
+    const novaFrase = frase
+        .replace(/ufat/g, 'u')
+        .replace(/ober/g, 'o')
+        .replace(/ai/g, 'a')
+        .replace(/imes/g, 'i')
+        .replace(/enter/g, 'e');
+    return novaFrase;
+}
+
+function receberFraseCriptografar() {
+    const frase = document.getElementById('entrada-texto').value;
+    document.getElementById('saida-texto').innerHTML = codificar(frase.toLowerCase());
+}
+
+function receberFraseDescriptografar() {
+    const frase = document.getElementById('entrada-texto').value;
+    document.getElementById('saida-texto').innerHTML = decodificar(frase.toLowerCase());
+}
+
+function copiarFrase() {
+    const frase = document.getElementById('saida-texto');
+    frase.select();
+    navigator.clipboard.writeText(frase.value);
+}
